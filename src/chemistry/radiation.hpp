3f13e73ddcbc843e2f4fc91642a2860cc9b9b129
Bob Caddy <rcaddy@princeton.edu>
2026-07-01 11:02:12 -0400

    feat: chem radiation file

---
+++ src/chemistry/radiation.hpp
@@ -0,0 +1,23 @@
+#ifndef CHEMISTRY_RADIATION_HPP_
+#define CHEMISTRY_RADIATION_HPP_
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file radiation.hpp
+//  \brief Interfaces to radiative transfer for the chemistry code
+
+#include "athena.hpp"
+#include "chemistry/chemistry_utils.hpp"
+#include "chemistry/thermo/thermo.hpp"
+#include "utils/register_array.hpp"
+
+namespace chemistry {
+class Radiation {
+ public:
+ private:
+};
+
+}  // namespace chemistry
+#endif  // CHEMISTRY_RADIATION_HPP_
