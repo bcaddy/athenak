17346dc19835b01e981c49ede260ecbd74826cc7
Robert Caddy <rcaddy@princeton.edu>
2026-09-11 15:36:41 -0400

    doc(ode): update comment in ODE solver to reflect upstream changes

--- src/ode_solvers/kokkos_bdf.hpp
+++ src/ode_solvers/kokkos_bdf.hpp
@@ -60,8 +60,8 @@ class KokkosBDF {
   const Real t_end;
   /// First time step size, if zero then the solver will decide
   const Real dt0;
-  /// The maximum time step, as of Kokkos Kernels 4.4 this is not implemented so
-  /// it does nothing
+  /// The maximum time step, if it's zero then the solver will decide. The
+  /// constructor sets it to dt
   const Real max_step;
 
   /*!
